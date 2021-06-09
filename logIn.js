5e36448188e8663a230f5b9d642003bb7b092e9a
Aleksandr <aleksandr.suslov95@gmail.com>
2021-06-09 23:54:24 +0400

    begin work on valid form

---
+++ logIn.js
@@ -0,0 +1,16 @@
+// const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+// const INPUT = document.querySelector('input');
+
+// function validateEmail(value) {
+//   return EMAIL_REGEXP.test(value);
+// }
+
+// function updateInput() {
+//     if (validateEmail(INPUT.value)) INPUT.style.borderColor = 'green';
+//         else INPUT.style.borderColor = 'red';
+// }
+// INPUT.addEventListener('input', updateInput);    
+let themeButton = document.querySelector('.LogIn-button');
+themeButton.onclick = function() {
+    
+};
