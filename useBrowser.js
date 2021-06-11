14a5c373617df2c609c96a04dfb5384f6a894fe9
Aleksandr <aleksandr.suslov95@gmail.com>
2021-06-11 22:38:56 +0400

    formatScript

--- useBrowser.js
+++ useBrowser.js
@@ -1,18 +1,24 @@
-	let userAgent = navigator.userAgent.toLowerCase();
-	let Mozila = /firefox/.test(userAgent);
-	let Chrome = /chrome/.test(userAgent);
-	let Safari = /safari/.test(userAgent);
-	let Opera  = /opera/.test(userAgent);
-	let message = document.querySelector('.useBrowser');
-	console.log(Mozila);
-    console.log(Chrome);
-    console.log(Safari);
-    console.log(Opera);
-	console.log(message);
-	let InternetExplorer = false;
-	if((/mozilla/.test(userAgent) && !/firefox/.test(userAgent) && !/chrome/.test(userAgent) && !/safari/.test(userAgent) && !/opera/.test(userAgent)) || /msie/.test(userAgent))
-		InternetExplorer = true;
-	if(InternetExplorer){
-		message.textContent = "Error: you use incorrect browser";
-	}
-    
+let userAgent = navigator.userAgent.toLowerCase();
+let Mozila = /firefox/.test(userAgent);
+let Chrome = /chrome/.test(userAgent);
+let Safari = /safari/.test(userAgent);
+let Opera = /opera/.test(userAgent);
+let message = document.querySelector(".useBrowser");
+console.log(Mozila);
+console.log(Chrome);
+console.log(Safari);
+console.log(Opera);
+console.log(message);
+let InternetExplorer = false;
+if (
+  (/mozilla/.test(userAgent) &&
+    !/firefox/.test(userAgent) &&
+    !/chrome/.test(userAgent) &&
+    !/safari/.test(userAgent) &&
+    !/opera/.test(userAgent)) ||
+  /msie/.test(userAgent)
+)
+  InternetExplorer = true;
+if (InternetExplorer) {
+  message.textContent = "Error: you use incorrect browser";
+}
